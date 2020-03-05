7722a9668918570d4bb6938880d4dbbf1efecac8
SimonLoir <simon-loir@hotmail.com>
2020-03-05 17:16:21 +0100

    added a way to download blobs

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -20,3 +20,33 @@ const test = async () => {
     window.fn = instance.exports;
 };
 test();
+
+const button = document.createElement('button');
+document.body.appendChild(button);
+button.innerText = 'Télécharger le fichier wasm';
+console.log(builder.export());
+button.addEventListener('click', () =>
+    downloadBlob(builder.export(), 'file.wasm', 'wasm')
+);
+
+function downloadBlob(data: any, fileName: string, mimeType: string) {
+    var blob: any, url: any;
+    blob = new Blob([data], {
+        type: mimeType,
+    });
+    url = window.URL.createObjectURL(blob);
+    downloadURL(url, fileName);
+    setTimeout(function() {
+        return window.URL.revokeObjectURL(url);
+    }, 1000);
+}
+
+function downloadURL(data: any, fileName: string) {
+    var a;
+    a = document.createElement('a');
+    a.href = data;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+}
