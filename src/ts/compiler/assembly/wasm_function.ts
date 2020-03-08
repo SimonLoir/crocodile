6492a3678c5b6b8dc49aade1c87005d85c90ee4a
SimonLoir <simon-loir@hotmail.com>
2020-03-08 13:24:34 +0100

    added some helper functions

--- src/ts/compiler/assembly/wasm_function.ts
+++ src/ts/compiler/assembly/wasm_function.ts
@@ -1,11 +1,35 @@
 import wasm_builder from './wasmbuilder';
 
+type type = 'i32' | 'f64' | 'i64' | 'f32';
 export default class wasm_function {
     constructor() {}
-    public static f32_sub(a: any[], b: any[]): any[] {
-        return [...a, ...b, wasm_builder.codes.sub_f32];
+
+    public static op(op: any, a: any[], b: any[]): any[] {
+        //@ts-ignore
+        return [...a, ...b, wasm_builder.codes[op]];
     }
-    public static f32_add(a: any[], b: any[]): any[] {
-        return [...a, ...b, wasm_builder.codes.add_f32];
+
+    public static add(type: type, a: any[], b: any[]): any[] {
+        return this.op('add_' + type, a, b);
+    }
+
+    public static sub(type: type, a: any[], b: any[]): any[] {
+        return this.op('sub_' + type, a, b);
+    }
+
+    public static mul(type: type, a: any[], b: any[]): any[] {
+        return this.op('mul_' + type, a, b);
+    }
+
+    public static div_signed(type: type, a: any[], b: any[]): any[] {
+        return this.op('div_s_' + type, a, b);
+    }
+
+    public static div_unsigned(type: type, a: any[], b: any[]): any[] {
+        return this.op('div_u_' + type, a, b);
+    }
+
+    public static div_float(type: type, a: any[], b: any[]): any[] {
+        return this.op('div_s_' + type, a, b);
     }
 }
